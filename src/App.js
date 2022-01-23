c30268b45482fe50149abaccfd9d52f205823480
LEO-Malay <malaybhavsar.290@gmail.com>
2022-01-23 21:14:55 +0530

    Auth folder, Css Optimiztion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Login from "./views/Auth/Login";
 const App = () => {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/Login" element={<Login />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/views/Auth/Login.js
@@ -0,0 +1,9 @@
+import "../../css/Login.css";
+const Login = () => {
+    return (
+        <div className="Login">
+            <h1>Please Login</h1>
+        </div>
+    );
+};
+export default Login;
